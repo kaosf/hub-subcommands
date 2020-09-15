16fdde78f58cadc0a16987569b63c5fa334ce375
ka <ka.kaosf@gmail.com>
2020-09-15 15:08:17 +0900

    Remove my "browse" sub command

--- hub-subcommands.sh
+++ hub-subcommands.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [[ $1 = browse ]]
-then
-  shift
-  $PREFIX/libexec/hub-subcommands/browse $@
-elif [[ $1 = repositories ]]
+if [[ $1 = repositories ]]
 then
   shift
   $PREFIX/libexec/hub-subcommands/repositories $@
